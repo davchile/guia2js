5432f007931f736a7e55dab1936320ef0f8492a2
David Paillacheo P <david.paillacheo@ticonsultores.cl>
2024-06-07 20:24:01 -0400

    password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
-edad = 14
+// edad = 4
+// if(edad<18) {
+//     console.log("Lo siento, No puede pasar al club")
+// }
 
-if(edad<18) {
-    console.log("Lo siento, No puede pasar al club")
-}
+// edad = prompt("Ingrese su edad")
+// if(edad<18) {
+    //     console.log("Lo siento, No puede pasar al club")
+    // }
+
+// edad = 4
+// if (edad < 18) {
+//     console.log("1")
+//     console.log("2")
+//     console.log("3")
+//     a = 2 + 2
+//     console.log(a)
+// }
+
+// temperatura = 30
+// if (temperatura >= 25) {
+//     console.log("Hace calor")
+// }
+
+// password = "secret"
+// if (password == "secret") {
+//     alert("conoces el password")
+// }
+
