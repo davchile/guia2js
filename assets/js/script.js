f0f84ef61fc20708460f77c1938f1a20af605eaf
David Paillacheo P <david.paillacheo@ticonsultores.cl>
2024-06-15 17:08:56 -0400

    tortas

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,17 +27,42 @@
 //     alert("conoces el password")
 // }
 
-const pass=document.querySelector("#password")  //declaro el input
-const btn=document.querySelector("#btn")    //declaro el boton para escucharlo
-const result=document.querySelector("#result")  //declaro el texto de resultado
+// const pass=document.querySelector("#password")  //declaro el input
+// const btn=document.querySelector("#btn")    //declaro el boton para escucharlo
+// const result=document.querySelector("#result")  //declaro el texto de resultado
 
-btn.addEventListener("click", ()=> {
-    const value = pass.value    //me traigo el valor del password
-    if(value === "secret") {    //agrego la condicion y la comparo con el input
-        result.innerHTML = "Lo lograste"    //genero el resultado
+// btn.addEventListener("click", ()=> {
+//     const value = pass.value    //me traigo el valor del password
+//     if(value === "secret") {    //agrego la condicion y la comparo con el input
+//         result.innerHTML = "Lo lograste"    //genero el resultado
+//     } else {
+//         result.innerHTML = "No lo lograste"
+//     }            
+// })
+
+// pedazosDeTorta = 3
+// invitados = 4
+
+// cantidadDePedazosPorInvitado = pedazosDeTorta / invitados
+
+// if(cantidadDePedazosPorInvitado >= 1) {
+//     alert("Alcanza para todos")
+// }
+//     else {
+//         alert("Anda a comprar mas torta por fa")
+// }
+
+const torta = document.querySelector("#torta")
+const invitados = document.querySelector("#invitados")
+const btn = document.querySelector("#calculate")
+
+btn.addEventListener("click", () => {
+    const tortaValue = torta.value
+    const invitadosValue = invitados.value
+    const tortaPorInvitado = tortaValue / invitadosValue
+    if (tortaPorInvitado >= 1) {
+        alert("Alcanza para todos")
     } else {
-        result.innerHTML = "No lo lograste"
-    }            
+        alert("Compra más torta pollo")
+    }
 })
-
-
